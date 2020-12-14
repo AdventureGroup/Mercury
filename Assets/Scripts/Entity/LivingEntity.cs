bb8bb33be7df0b33c1a9803bf970145d796054aa
YumisLink <57590359+YumisLink@users.noreply.github.com>
2020-12-14 12:46:31 +0800

    创建了entity以及其子类
    1

---
+++ Assets/Scripts/Entity/LivingEntity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingEntity : Entity
+{
+    public float Health;
+    public float HealthRec;
+    public float Velocity;
+    public float Direction;
+
+    public virtual void Healing()
+    {
+        Health += HealthRec;
+    }
+    public virtual void Death()
+    {
+        
+    }
+    private void Update()
+    {
+        Healing();
+        if (Health <= 0)
+            Death();
+    }
+   
+
+}
---
+++ Assets/Scripts/Entity/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : LivingEntity
+{
+    private float createTime;
+    public override void Death()
+    {
+        Object.Destroy(this);
+    }
+    void Init()
+    {
+        createTime = Time.time + 120;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (createTime < Time.time)
+        {
+            Death();
+        }
+    }
+}
